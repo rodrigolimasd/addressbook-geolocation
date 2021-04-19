625437d962856255bc4daf487c30dd25b71ed880
Rodrigo Lima <rodrigolimasd@gmail.com>
2021-04-19 17:34:17 -0300

    Initial commit

---
+++ src/test/java/com/stoom/addressbook/AddressbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoom.addressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
